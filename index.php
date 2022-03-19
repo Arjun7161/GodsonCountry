7e01d99fdc513554c2ec4066ac8bd845ed1e889d
Arjun7161 <akarjun7161@gmail.com>
2022-03-19 11:14:53 +0000

    index

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
 <?php
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="high">
+        <video class="video" autoplay loop muted plays-incline>
+            <source src="Images/istockphoto-472967407-640_adpp_is.mp4" type="video/mp4">
+        </video>
+        <nav>
+            <img src="Images/logo.png" class="logo">
+            <a class="navbar-brand" href="#">God's Own Country</a>
+
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Log In</a></li>
+                <li><a href="#">Sign Up</a></li>
+            </ul>
+        </nav>
+        <div class="content">
+            <h1>What is Lorem Ipsum?
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen book.</h1>
+
+        </div>
+    </div>
+    <footer>
+        <p>Copyright Team I | 2022 | Gods Own Country</p>
+    </footer>
+</body>
+
+</html>
