d5e0b69ef8338698dbcf672f2dea3f845c60a913
Arjun7161 <akarjun7161@gmail.com>
2022-03-21 15:32:17 +0000

    location

--- location.php
+++ location.php
@@ -8,18 +8,20 @@ if(!isset($_SESSION['username'])){
 
 ?>
 
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Location</title>
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-    <div class="high">
+<div class="high">
         <video class="video" autoplay loop muted plays-incline>
             <source src="Img/istockphoto-1366247940-640_adpp_is.mp4" type="video/mp4">
         </video>
@@ -40,7 +42,7 @@ if(!isset($_SESSION['username'])){
         </div>
         
         <div class="content">
-            <div class="col">
+        <div class="col">
              <h1>Kerala</h1>
             <p1>What is Lorem Ipsum?
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
@@ -54,6 +56,14 @@ if(!isset($_SESSION['username'])){
                     <div class="card card2">
                         <a href="architect.php" class="button">Architect</a>
                     </div>
+                    <div class="card card2">
+                        <a href="architect.php" class="button">Architect</a>
+                    </div>
+                    <div class="card card2">
+                        <a href="architect.php" class="button">Architect</a>
+                    </div>
+                   
+
                 </div>
                 </div>
     </div>
@@ -61,5 +71,4 @@ if(!isset($_SESSION['username'])){
         <p>Copyright CMM007 | 2022 | Gods Own Country</p>
     </footer>
 </body>
-
 </html>
