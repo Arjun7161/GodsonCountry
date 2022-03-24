7ea451c52a99855b479abebaae5fbdcb930a13b5
Arjun7161 <akarjun7161@gmail.com>
2022-03-24 20:42:29 +0000

    upload

--- upload.php
+++ upload.php
@@ -11,9 +11,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload</title>
+    <title>Upload file</title>
 </head>
 <body>
-    
+    <form action="" method="post" autocomplete="off"></form>
 </body>
 </html>
