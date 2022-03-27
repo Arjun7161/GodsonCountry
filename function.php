e045552dc67a9cdc777c2932e2919841a205c9d8
Arjun7161 <akarjun7161@gmail.com>
2022-03-27 17:08:24 +0100

    del

--- function.php
+++ function.php
@@ -66,3 +66,85 @@ if(!empty($_POST['title']) && !empty($_POST['content'])){
 }
 
 }
+
+//update function to get the data
+
+function update_get(){
+    if(isset($_GET['id']) && is_numeric($_GET['id'])){
+        // connection
+        global $con;
+        // id url to verify by te id number
+        $id=$_GET['id'];
+        // select all data from the get info from url id
+        $get_id=mysqli_query($con, "SELECT * FROM story WHERE id='$id'");
+        if(mysqli_num_rows($get_id)===1){
+            $row=mysqli_fetch_assoc($get_id);
+            return($row);
+        }
+    }
+}
+
+if(isset($_POST['update_title']) && isset($_POST['update_content'])){
+
+    // checking if text areas are blank
+    if(!empty($_POST['update_title'])  && !empty($_POST['update_content'])){
+
+        $title=mysqli_real_escape_string($con, htmlspecialchars($_POST['update_title']));
+        $content=mysqli_real_escape_string($con, htmlspecialchars($_POST['update_content']));
+
+        $id=$_GET['id'];
+
+        $update_query=mysqli_query($con, "UPDATE story SET title='$title',content='$content' WHERE id=$id");
+
+        if($update_query){
+            echo "<script>alert('Updated');window.location.href='storylist.php';</script>";
+            exit;
+        }else{
+            echo "<h3>Error updating</h3>";
+        }
+
+    }else{
+        echo "<h4>Fill all the fields</h4>";
+    }
+}
+
+// Delete function
+function delete(){
+    global $con;
+    if(isset($_GET['id']) && is_numeric($_GET['id'])){
+        $userid=$_GET['id'];
+        $deleted=mysqli_query($con, "DELETE FROM story WHERE id='$userid'");
+
+        if($deleted){
+            echo "<script>alert('Data deleted');window.location.href='delete.php';</script>";
+            exit;
+        }else{
+            echo "Error while deleting";
+        }
+    }
+}
+
+function get_all_edit_data(){
+    global $con;
+    $get_data=mysqli_query($con, "SELECT * FROM story");
+    if(mysqli_num_rows($get_data)>0){
+        echo '<table>
+        <tr>
+        <th><h2>Edit Data</h2></th>
+        </tr>';
+        while($row=mysqli_fetch_assoc($get_data)){
+            echo '<tr>
+            <td>'.$row['title'].'</td>
+            <td>
+            <a href="update.php?id='.$row['id'].'">Edit</a>
+            |
+            <a href="delete.php?id='.$row['id'].'">Delete</a>
+            </td>
+            </tr>';
+        }
+        echo '</table>';
+    }else{
+        echo"<h3>Add story</h3>";
+
+    }
+}
